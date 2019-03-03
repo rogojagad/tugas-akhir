b87612afd58536e63761c9bed6a8783564e7d2cb
rogojagad <rogojagadalit@gmail.com>
2019-03-03 16:11:48 +0700

    store prediction result along with pos tag lable + remove pos tag generator

--- aspect_term_detection.py
+++ aspect_term_detection.py
@@ -5,6 +5,7 @@ from custom_utils import *
 
 data_dir = "D:\Kuliah\TA\data"
 
+
 def load_train_data():
     with open(data_dir + "\\train\\features.pickle", "rb") as inp:
         feature = pickle.load(inp)
@@ -14,19 +15,17 @@ def load_train_data():
 
     return feature, labels
 
+
 def train(X_train, y_train):
     trainer = pycrfsuite.Trainer()
 
     for xseq, yseq in zip(X_train, y_train):
         trainer.append(xseq, yseq)
 
-    trainer.set_params({
-        'c1' : 0.1,
-        'c2' : 0.01,
-        'feature.possible_transitions': True,
-    })
+    trainer.set_params({"c1": 0.1, "c2": 0.01, "feature.possible_transitions": True})
 
-    trainer.train('crf.model')
+    trainer.train("crf.model")
+
 
 def load_test_data():
     with open(data_dir + "\\test\\features.pickle", "rb") as inp:
@@ -37,18 +36,20 @@ def load_test_data():
 
     return feature, labels
 
+
 def predict(X_test):
     print("=========== NOW DETECTING ASPECT TERM ON TEST DATA ===============\n")
 
-    tagger = pycrfsuite.Tagger('crf.model')
+    tagger = pycrfsuite.Tagger("crf.model")
 
-    tagger.open('crf.model')
-    
+    tagger.open("crf.model")
+
     result = [tagger.tag(xseq) for xseq in X_test]
 
     print("===========               FINISH                   ===============\n")
 
     return result
+
 
 def make_result(y_result):
     with open(data_dir + "\\test\\labelled_words.pickle", "rb") as inp:
@@ -61,13 +62,15 @@ def make_result(y_result):
 
         for i in range(len(data[0])):
             word = data[0][i][0]
+            pos = data[0][i][1]
             label = data[1][i]
 
-            temp.append((word, label))
-        
+            temp.append((word, pos, label))
+
         prediction_result.append(temp)
 
     return prediction_result
+
 
 if __name__ == "__main__":
     xseq, yseq = load_train_data()
