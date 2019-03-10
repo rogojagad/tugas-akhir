47b018324e8eb8ccab9d9ca92c6eeb20ee6e12b0
rogojagad <rogojagadalit@gmail.com>
2019-03-10 15:19:09 +0700

    change searching method

--- bing_liu_lexicon_value_getter.py
+++ bing_liu_lexicon_value_getter.py
@@ -16,20 +16,10 @@ class BingLiuLexiconValueGetter:
             self.negative_lex = pickle.load(inp)
 
     def get_value(self, token):
-        if self.is_positive(token):
+        if token in self.positive_lex:
             return 1
-        elif self.is_negative(token):
+        elif token in self.negative_lex:
             return -1
-
-    def is_positive(self, word):
-        for token in self.positive_lex:
-            if token == word:
-                return True
-
-    def is_negative(self, word):
-        for token in self.negative_lex:
-            if token == word:
-                return True
 
 
 if __name__ == "__main__":
--- gi_lexicon_value_getter.py
+++ gi_lexicon_value_getter.py
@@ -11,22 +11,11 @@ class GeneralInquirerLexiconValueGetter:
         with open(data_dir + "/general-inquirer/negative-words.pickle", "rb") as inp:
             self.negative_lex = pickle.load(inp)
 
-    def get_value(self, token):
-        if self.is_positive(token):
+     def get_value(self, token):
+        if token in self.positive_lex:
             return 1
-        elif self.is_negative(token):
+        elif token in self.negative_lex:
             return -1
-
-    def is_positive(self, word):
-        for token in self.positive_lex:
-            if token == word:
-                return True
-
-    def is_negative(self, word):
-        for token in self.negative_lex:
-            if token == word:
-                return True
-
 
 if __name__ == "__main__":
     getter = GeneralInquirerLexiconValueGetter()
