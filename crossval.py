19475ec4ede7714c61128b5f701daff2fa62ea3d
rogojagad <rogojagadalit@gmail.com>
2019-03-25 15:13:17 +0700

    change / optimizing hyperparameter

--- crossval.py
+++ crossval.py
@@ -4,6 +4,7 @@ import sklearn_crfsuite
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn_crfsuite import metrics
+from sklearn.model_selection import KFold
 
 data_dir = "D:\Kuliah\TA\data"
 
@@ -19,19 +20,35 @@ with open(data_dir + "\\test\\features.pickle", "rb") as inp:
 with open(data_dir + "\\test\\labels.pickle", "rb") as inp:
     y_test = pickle.load(inp)
 
+X = X_train + X_test
+y = y_train + y_test
+
 crf = sklearn_crfsuite.CRF(algorithm="lbfgs", all_possible_transitions=True)
 
 params_space = {"c1": scipy.stats.expon(scale=0.5), "c2": scipy.stats.expon(scale=0.05)}
 
 f1_scorer = make_scorer(metrics.flat_f1_score, average="weighted")
 
-rs = RandomizedSearchCV(
-    crf, params_space, verbose=1, n_jobs=-1, n_iter=50, scoring=f1_scorer
-)
+k = KFold(n_splits=5, shuffle=False)
 
-rs.fit(X_train, y_train)
+best_scores = []
 
-print("Best params : ", rs.best_params_)
-print("Best CV score : ", rs.best_score_)
-print("model size: {:0.2f}M".format(rs.best_estimator_.size_ / 1000000))
+best_params = []
 
+for _ in range(2):
+    rs = RandomizedSearchCV(
+        crf, params_space, verbose=1, n_jobs=-1, n_iter=50, scoring=f1_scorer, cv=k
+    )
+
+    rs.fit(X, y)
+
+    best_scores.append(rs.best_score_)
+
+    best_params.append(rs.best_params_)
+
+# print("Best params : ", rs.best_params_)
+# print("Best CV score : ", rs.best_score_)
+# print("model size: {:0.2f}M".format(rs.best_estimator_.size_ / 1000000))
+
+print(best_scores, end="/n/n")
+print(best_params)
