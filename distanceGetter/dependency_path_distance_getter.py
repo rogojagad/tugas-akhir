480c4678f42841843364ac957e8ec3014decc8b3
rogojagad <rogojagadalit@gmail.com>
2019-04-17 22:58:47 +0700

    error handler

--- distanceGetter/dependency_path_distance_getter.py
+++ distanceGetter/dependency_path_distance_getter.py
@@ -10,6 +10,8 @@ class DependencyPathDistanceGetter:
         ) as inp:
             self.edges_list = pickle.load(inp)
 
+        print("Dependency Path Distance Getter instantiated...")
+
     def get_distance_value(self, source, source_idx, target, target_idx, sentence_idx):
         """
         Parameters: source token, source token index, target token, target token index, sentence index
@@ -20,10 +22,16 @@ class DependencyPathDistanceGetter:
         source += "-" + str(source_idx)
         target += "-" + str(target_idx)
 
-        return nx.shortest_path_length(graph, source=source, target=target)
+        try:
+            return nx.shortest_path_length(graph, source=source, target=target)
+        except nx.exception.NetworkXNoPath:
+            return 1
+        except nx.exception.NodeNotFound:
+            return 1
 
 
 if __name__ == "__main__":
     getter = DependencyPathDistanceGetter()
 
-    print(getter.get_distance_value("amazing", 3, "service", 4, 100))
+    # print(getter.get_distance_value("amazing", 3, "service", 4, 100))
+    pprint(getter.edges_list[241])
--- valueGetter/mpqa_value_getter.py
+++ valueGetter/mpqa_value_getter.py
@@ -6,23 +6,33 @@ class MPQALexiconValueGetter:
         with open("D:\Kuliah\TA\data\mpqa_lexicon\lexicon.json") as f:
             self.lexicon = json.load(f)
 
-    def get_value(self, word):
-        subjectivity = self.lexicon[word]["subjectivity"]
-        polarity = self.lexicon[word]["polarity"]
+        print("MPQA Lexicon Value Getter instantiated...")
 
-        if subjectivity == "strongsubj":
-            if polarity == "positive":
-                return 1
-            elif polarity == "negative":
-                return -1
-        elif subjectivity == "weaksubj":
-            if polarity == "positive":
-                return 0.5
-            elif polarity == "negative":
-                return -0.5
+    def get_value(self, word):
+        try:
+            subjectivity = self.lexicon[word]["subjectivity"]
+            polarity = self.lexicon[word]["polarity"]
+
+            if subjectivity == "strongsubj":
+                if polarity == "positive":
+                    return 1
+                elif polarity == "negative":
+                    return -1
+                else:
+                    return 0
+            elif subjectivity == "weaksubj":
+                if polarity == "positive":
+                    return 0.5
+                elif polarity == "negative":
+                    return -0.5
+                else:
+                    return 0
+
+        except KeyError:
+            return 0
 
 
 if __name__ == "__main__":
     getter = MPQALexiconValueGetter()
 
-    print(getter.get_value("above"))
+    print(getter.get_value("complete"))
